72649c38cb0214fcb5462548433616f00930fa94
blackmiaool <blackmiaool@gmail.com>
2017-08-23 21:03:59 +0800

    support style's combination

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const postcss = require('postcss');
 
 const configSelector = 'rn-config';
 
-function traverse(obj, cb, path) {
+function traverse(obj, cb) {
     function handle(v, key) {
         if (typeof v == 'object' && v) {
             path.push(key);
@@ -13,27 +13,39 @@ function traverse(obj, cb, path) {
             return cb(v, key,path);
         }
     }
-    if (Array.isArray(obj)) {
-        obj.filter(function (v, i) {
-            const result = handle(v, i);
-            if (result === undefined) {
-                return false;
-            } else {
-                obj[i] = result;
-                return true;
-            }
-        });
-    } else {
-        for (const key in obj) {
-            const result = handle(obj[key], key);
-            if (result === undefined) {
-                delete obj[key];
-            } else {
-                obj[key] = result;
-            }
 
+    for (const component in JSONResult) {
+        for (const key in JSONResult[component]) {
+            const arr = JSONResult[component][key];
+            arr.forEach((chunk)=>{
+                for(const key in chunk.style){
+                    const value=chunk.style[key];
+                    cb(key,value,selectors);
+                }
+            });
         }
     }
+    // if (Array.isArray(obj)) {
+    //     obj.filter(function (v, i) {
+    //         const result = handle(v, i);
+    //         if (result === undefined) {
+    //             return false;
+    //         } else {
+    //             obj[i] = result;
+    //             return true;
+    //         }
+    //     });
+    // } else {
+    //     for (const key in obj) {
+    //         const result = handle(obj[key], key);
+    //         if (result === undefined) {
+    //             delete obj[key];
+    //         } else {
+    //             obj[key] = result;
+    //         }
+
+    //     }
+    // }
 }
 module.exports = function ({code:rawCode,before,after}) {
     const input = JSON.parse(rawCode);
@@ -51,9 +63,9 @@ module.exports = function ({code:rawCode,before,after}) {
     function addStep(func){
         steps.push(func);
     }
-    addStep(function(value,property){
+    addStep(function(value,property,selectors){
         const regexpArr = args.split(',').map(arg => arg.trim()).map(name => new RegExp(`(^|['"])` + name + "([\\[\\.]|$)"));
-        console.log("!",value,property)
+        console.log("!",value,property,selectors)
         if (typeof value === 'string') {
             if (regexpArr.some((regexp) => regexp.test(value))) {
                 value = value.replace(/^['"]/, '').replace(/['"]$/, '');
@@ -63,11 +75,11 @@ module.exports = function ({code:rawCode,before,after}) {
     });
     console.log(JSON.stringify(input));
     steps.forEach((func)=>{
-        traverse(input, function (value, key,path) {
+        traverseStyle(input, function (value, key,selectors) {
             if(typeof key==='number'){
                 return ;
             }
-            const result=func(value,key);
+            const result=func(value,key,selectors);
             if(result!==undefined){
                 return `[[[${result}]]]`
             }else{
