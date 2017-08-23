f816318b25f81f7915f5b16efd00a69ff9d5d294
blackmiaool <blackmiaool@gmail.com>
2017-08-23 21:21:24 +0800

    update traverser

--- src/index.js
+++ src/index.js
@@ -2,52 +2,31 @@ const postcss = require('postcss');
 
 const configSelector = 'rn-config';
 
-function traverse(obj, cb) {
-    function handle(v, key) {
-        if (typeof v == 'object' && v) {
-            path.push(key);
-            traverse(v, cb,path);
-            path.pop();
-            return v;
-        } else {
-            return cb(v, key,path);
-        }
-    }
-
-    for (const component in JSONResult) {
-        for (const key in JSONResult[component]) {
-            const arr = JSONResult[component][key];
-            arr.forEach((chunk)=>{
-                for(const key in chunk.style){
-                    const value=chunk.style[key];
-                    cb(key,value,selectors);
+function traverseStyle(root, handleStyle, handleProperty) {
+    for (const component in root) {
+        for (const selector in root[component]) {
+            const arr = root[component][selector];
+            arr.forEach((chunk) => {
+                chunk.style = handleStyle(chunk.style);
+                for (const key in chunk.style) {
+                    const value = chunk.style[key];
+                    const selectors = chunk.selectors.concat([selector, component]);
+                    const result = handleProperty(value, key, selectors);
+                    if (result === undefined) {
+                        delete chunk.style[key];
+                    } else {
+                        chunk.style[key] = result;
+                    }
                 }
             });
         }
     }
-    // if (Array.isArray(obj)) {
-    //     obj.filter(function (v, i) {
-    //         const result = handle(v, i);
-    //         if (result === undefined) {
-    //             return false;
-    //         } else {
-    //             obj[i] = result;
-    //             return true;
-    //         }
-    //     });
-    // } else {
-    //     for (const key in obj) {
-    //         const result = handle(obj[key], key);
-    //         if (result === undefined) {
-    //             delete obj[key];
-    //         } else {
-    //             obj[key] = result;
-    //         }
-
-    //     }
-    // }
 }
-module.exports = function ({code:rawCode,before,after}) {
+module.exports = function ({
+    code: rawCode,
+    before,
+    after
+}) {
     const input = JSON.parse(rawCode);
     // console.log(input);
     const ret = {};
@@ -57,15 +36,16 @@ module.exports = function ({code:rawCode,before,after}) {
         delete input[configSelector];
     }
     let args = config.arguments || '';
-    
 
-    let steps=[];
-    function addStep(func){
+
+    let steps = [];
+
+    function addStep(func) {
         steps.push(func);
     }
-    addStep(function(value,property,selectors){
+    addStep(function (value, property, selectors) {
         const regexpArr = args.split(',').map(arg => arg.trim()).map(name => new RegExp(`(^|['"])` + name + "([\\[\\.]|$)"));
-        console.log("!",value,property,selectors)
+        console.log("!", value, property, selectors)
         if (typeof value === 'string') {
             if (regexpArr.some((regexp) => regexp.test(value))) {
                 value = value.replace(/^['"]/, '').replace(/['"]$/, '');
@@ -74,20 +54,19 @@ module.exports = function ({code:rawCode,before,after}) {
         }
     });
     console.log(JSON.stringify(input));
-    steps.forEach((func)=>{
-        traverseStyle(input, function (value, key,selectors) {
-            if(typeof key==='number'){
-                return ;
-            }
-            const result=func(value,key,selectors);
-            if(result!==undefined){
+    steps.forEach((func) => {
+        traverseStyle(input, function (style, selectors) {
+            return style;
+        }, function (value, key, selectors) {
+            const result = func(value, key, selectors);
+            if (result !== undefined) {
                 return `[[[${result}]]]`
-            }else{
+            } else {
                 return value;
-            }            
-        },[]);
+            }
+        }, []);
     });
-    
+
     let result = JSON.stringify(input, false, 4);
     result = result.replace(/"\[\[\[/g, '')
         .replace(/\]\]\]"/g, '')
