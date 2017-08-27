ba1f907c00dc55e91a2dd0ec1e531c274b38c9dc
blackmiaool <blackmiaool@gmail.com>
2017-08-27 09:58:15 +0800

    fix style key

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ function traverseChunk(root, cb) {
     for (const component in root) {
         for (const styleStr in root[component]) {
             let arr = root[component][styleStr];
-            root[component][styleStr]=arr.map(function (chunk,i) {
+            root[component][styleStr] = arr.map(function (chunk, i) {
                 const result = cb(chunk, styleStr, component);
                 if (result !== undefined) {
                     return result;
-                }else{
+                } else {
                     return chunk;
                 }
             });
@@ -42,7 +42,7 @@ function traverseProperty(root, func) {
             let result = func(value, key, selector);
             if (result === null) {
                 delete style[key];
-            }else if(result===undefined){
+            } else if (result === undefined) {
                 //do nothing
             } else {
                 result = `[[[${result}]]]`;
@@ -70,14 +70,16 @@ module.exports = function ({
     let args = config.arguments || '';
 
     //flatten style
-    if (!useHierarchy) { 
+    if (!useHierarchy) {
         for (const component in input) {
             for (const styleStr in input[component]) {
                 const arr = input[component][styleStr];
                 input[component][styleStr] = [{
-                    selector:[],
-                    style: arr.reduce(function (p,{style}) {
-                        Object.assign(p,style);
+                    selector: [],
+                    style: arr.reduce(function (p, {
+                        style
+                    }) {
+                        Object.assign(p, style);
                         return p;
                     }, {})
                 }];
@@ -88,7 +90,7 @@ module.exports = function ({
     //sort property
     traverseStyle(input, function ({
         style
-    }) { 
+    }) {
         const ret = {};
         Object.keys(style).sort().forEach((key) => {
             ret[key] = style[key];
@@ -97,7 +99,7 @@ module.exports = function ({
     });
 
     //make variables work
-    traverseProperty(input, function (value, property, selector) { 
+    traverseProperty(input, function (value, property, selector) {
         const regexpArr = args.split(',').map(arg => arg.trim()).map(name => new RegExp(`(^|['"])` + name + "([\\[\\.\"]|$)"));
         if (typeof value === 'string') {
             if (regexpArr.some((regexp) => regexp.test(value))) {
@@ -111,26 +113,26 @@ module.exports = function ({
     const styleSheetArr = [];
     traverseStyle(input, function ({
         style
-    }) { 
+    }) {
         const str = JSON.stringify(style);
         const index = styleSheetArr.indexOf(str);
         if (index === -1) {
-            return `allStyle[${styleSheetArr.push(str) - 1}]`;
+            return `allStyle.s${styleSheetArr.push(str) - 1}`;
         } else {
             return `allStyle[${index}]`;
         }
     });
     const styleSheetObj = {};
     styleSheetArr.forEach((style, index) => {
-        styleSheetObj[index] = JSON.parse(style);
+        styleSheetObj['s' + index] = JSON.parse(style);
     });
     codeBeforeReturn = `const allStyle = StyleSheet.create(${JSON5.stringify(styleSheetObj,false,4)});`
 
     //flatten style
-    if (!useHierarchy) { 
+    if (!useHierarchy) {
         for (const component in input) {
             for (const styleStr in input[component]) {
-                input[component][styleStr]=input[component][styleStr][0].style;
+                input[component][styleStr] = input[component][styleStr][0].style;
             }
         }
     }
@@ -140,15 +142,15 @@ module.exports = function ({
     let code = `
 const { StyleSheet } = require('react-native');
 
-module.exports= function({${args}}){
+module.exports = function({${args}}){
     ${codeBeforeReturn.replace(/\n/g, '\n    ')}
 
-    return ${result}
-}
+    return ${result};
+};
 `;
     code = code.replace(/"\[\[\[/g, '')
         .replace(/\]\]\]"/g, '');
 
-    console.log(code);
+    // console.log(code);
     return code;
 }
